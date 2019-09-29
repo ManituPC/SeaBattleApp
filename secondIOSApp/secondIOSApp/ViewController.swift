11767b9ee98c02955dbd2beea036f731975b0fa0
denis.chicherin <denis.chicherin@ignited.it>
2019-09-29 18:02:50 +0300

    init project

---
+++ secondIOSApp/secondIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondIOSApp
+//
+//  Created by denis.chicherin on 9/29/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
