f069991bb83dcae37698554e3dca0e1cc641acce
ManituPC <chicherin.dn@gmail.com>
2019-10-01 23:27:46 +0300

    add storyboards

---
+++ secondIOSApp/secondIOSApp/Screens/Loader/LoaderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoaderViewController.swift
+//  secondIOSApp
+//
+//  Created by denis.chicherin on 10/1/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class LoaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
